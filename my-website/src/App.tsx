9258adac485ea5655748ffad09f5fb1d89df92a7
CJ Barreiro <cjbarreiro@vt.edu>
2025-07-22 16:32:42 -0400

    website initialization

---
+++ my-website/src/App.tsx
@@ -0,0 +1,15 @@
+// my-website/src/App.tsx
+// Main application component for the website
+
+import Header from "./components/Header.tsx";
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-website/src/components/Header.tsx
@@ -0,0 +1,14 @@
+// my-website/src/Header.tsx
+// Header component for the website
+
+function Header() {
+
+    return (
+        <header>
+            <image />
+            <h1>CJ Barreiro</h1>
+        </header>
+    );
+}
+
+export default Header;
